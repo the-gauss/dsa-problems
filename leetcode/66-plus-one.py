58d809486d4a991f0da9195ce92dba0c4317548a
Kartik Kumar <kartikthory@icloud.com>
2026-07-22 21:22:23 +0530

    leetcode 50 and 69

--- leetcode/66-plus-one.py
+++ leetcode/66-plus-one.py
@@ -5,7 +5,7 @@ https://leetcode.com/problems/plus-one/?envType=study-plan-v2&envId=top-intervie
 The following solution runs in O(n) time and uses O(1) memory except the +[1]. This is
 the standard solution, and uses the same addition algorithm taught in elementary school.
 """
-def plusOne(self, digits: List[int]) -> List[int]:
+def plusOne(self, digits: list[int]) -> list[int]:
     for i in range(len(digits)-1, -1, -1):
         if digits[i]!=9:
             digits[i]+=1
